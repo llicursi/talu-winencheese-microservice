9161189264bfa967a698d1ebd87d4378a2188871
Zinsly, Tatiane (tz574u) <tz574u@att.com>
2018-07-21 18:28:47 -0300

    talu-client directory became a Maven project

---
+++ talu-client/src/main/java/com/br/talu/microservices/App.java
@@ -0,0 +1,13 @@
+package com.br.talu.microservices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
