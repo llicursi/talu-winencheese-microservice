a8af6171032fb1d79e2838e5afb57bba29e52a54
Zinsly, Tatiane (tz574u) <tz574u@att.com>
2018-10-07 22:16:15 -0300

    Add Autowired discovery client to look for URI's from other services dinamically

--- talu-client/src/main/java/com/br/talu/microservices/client/consumer/EurekaClientApp.java
+++ talu-client/src/main/java/com/br/talu/microservices/client/consumer/EurekaClientApp.java
@@ -1,16 +1,34 @@
 package com.br.talu.microservices.client.consumer;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.net.URI;
+import java.util.List;
+
 @SpringBootApplication
-@EnableDiscoveryClient()
+@EnableDiscoveryClient
 
 public class EurekaClientApp {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApp.class, args);
+    }
+
+    /**
+        Looking URI's from other services dinamically
+     */
+    @Autowired DiscoveryClient client;
+    public URI wineServiceUrl(){
+        List<ServiceInstance> list = client.getInstances("WINERIES");
+        if (list != null && list.size() > 0 ) {
+            return list.get(0).getUri();
+        }
+        return null;
     }
 }
 
