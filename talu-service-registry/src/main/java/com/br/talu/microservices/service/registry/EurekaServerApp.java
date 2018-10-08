2ea45bdadbeeb93bbd81bf73eba552409a982e84
Zinsly, Tatiane (tz574u) <tz574u@att.com>
2018-10-07 21:11:02 -0300

    Fix pom to use parent boot, dependency management and spring cloud eureka server

--- talu-service-registry/src/main/java/com/br/talu/microservices/service/registry/EurekaServerApp.java
+++ talu-service-registry/src/main/java/com/br/talu/microservices/service/registry/EurekaServerApp.java
@@ -7,10 +7,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 
-public class EurekaServiceApp {
+public class EurekaServerApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServiceApp.class, args);
+        SpringApplication.run(EurekaServerApp.class, args);
     }
 
 }
